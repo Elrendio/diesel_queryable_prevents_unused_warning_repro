4c6b41fc4a193f58a6b5a2e57e338a505ad3c080
Elrendio <oscar.walter@ens.fr>
2024-10-04 11:14:49 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// This has no warnings
+#[derive(diesel::Queryable, serde::Serialize)]
+struct Unused {
+    id: i32,
+}
+
+// This has warning: id is not used
+#[derive(diesel::Queryable)]
+struct UnusedWithFieldWarning {
+    id: i32,
+}
+
+// This has warning: id is not used
+#[derive(serde::Serialize)]
+struct UnusedWithStructWarning {
+    id: i32,
+}
+
+// This has warning: id is not used
+struct UnusedWithWarnings {
+    id: i32,
+}
+
+// This has warning: id is not used
+#[derive(Default)]
+struct WithDefaultDerive;
